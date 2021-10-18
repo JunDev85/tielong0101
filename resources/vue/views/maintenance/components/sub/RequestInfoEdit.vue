98bb1230cf2c34445b2a9dea29c77c5e0f268eda
kstar0101 <canopus0410@gmail.com>
2021-10-18 17:18:32 +0900

    middle_bigconnect category_id

--- resources/vue/views/maintenance/components/sub/RequestInfoEdit.vue
+++ resources/vue/views/maintenance/components/sub/RequestInfoEdit.vue
@@ -291,7 +291,7 @@ export default {
       }
       maintenanceResource.middle_bigconnect(this.mid_ca).then(res =>{
           // this.categories = res;
-          this.big_ca = res[0].category_name;
+          this.big_ca = res[0].category_id;
       }); 
     },
 

a6efafb3ef88cda60afdce9a9bf7081f373f77b5
kstar0101 <canopus0410@gmail.com>
2021-10-24 23:48:20 +0900

    commit

--- app/Http/Controllers/V2/MaintenanceController.php
+++ app/Http/Controllers/V2/MaintenanceController.php
@@ -448,7 +448,7 @@ class MaintenanceController extends Controller
         $qphotofile = Uploading_files::select('file_name')->where('kind', 'quotation_photo')
         ->where('info_id', $quotation_info_id)->first();
         var_export($quotation_info_id);
-        var_export($qphotofile); die;
+        var_export($qphotofile->file_name); die;
         $qphoto = $qphotofile->file_name;
 
         $quotationfile = Uploading_files::select('file_name')->where('kind', 'quotation')
